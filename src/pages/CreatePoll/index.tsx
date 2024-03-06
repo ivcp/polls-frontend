89a71a5849b2f7e2ce9cd04a44f7b025eb347b81
ivica <blizujemars@gmail.com>
2024-03-06 10:38:09 +0100

    style form

--- src/pages/CreatePoll/index.tsx
+++ src/pages/CreatePoll/index.tsx
@@ -5,17 +5,21 @@ import {
   Button,
   Group,
   ActionIcon,
+  Title,
+  Accordion,
+  Textarea,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { DateTimePicker } from '@mantine/dates';
 import { IconTrash } from '@tabler/icons-react';
+import classes from './index.module.css';
 
 const CreatePoll = () => {
   const form = useForm({
     initialValues: {
       question: '',
       description: '',
-      options: [{ value: '' }],
+      options: [{ value: '' }, { value: '' }],
       expiresAt: '',
       isPrivate: false,
       resultsVisibility: 'always',
@@ -38,60 +42,88 @@ const CreatePoll = () => {
   });
 
   return (
-    <form
-      onSubmit={form.onSubmit((values) => {
-        console.log(values);
-      })}
-    >
-      <TextInput
-        label="Question"
-        withAsterisk
-        {...form.getInputProps('question')}
-      />
-      <TextInput label="Description" {...form.getInputProps('description')} />
+    <>
+      <Title order={2} className={classes.title}>
+        Create a poll
+      </Title>
+      <form
+        className={classes.form}
+        onSubmit={form.onSubmit((values) => {
+          console.log(values);
+        })}
+      >
+        <TextInput
+          label="Question"
+          withAsterisk
+          {...form.getInputProps('question')}
+        />
+        <Textarea
+          autosize
+          label="Description"
+          {...form.getInputProps('description')}
+        />
 
-      {form.values.options.map((_, i) => (
-        <Group key={i} align="flex-end">
-          <TextInput
-            label={`Option ${i + 1}`}
-            withAsterisk={i > 1 ? false : true}
-            {...form.getInputProps(`options.${i}.value`)}
-          />
-          {i > 1 && (
-            <ActionIcon
-              size={'1.8rem'}
-              mb={'0.2rem'}
-              color="red.6"
-              onClick={() => form.removeListItem('options', i)}
-            >
-              <IconTrash />
-            </ActionIcon>
-          )}
-        </Group>
-      ))}
-      <Button onClick={() => form.insertListItem('options', { value: '' })}>
-        Add option
-      </Button>
+        {form.values.options.map((_, i) => (
+          <Group key={i} align="flex-end">
+            <Textarea
+              autosize
+              className={classes.optionInput}
+              label={`Option ${i + 1}`}
+              withAsterisk={i > 1 ? false : true}
+              {...form.getInputProps(`options.${i}.value`)}
+            />
+            {i > 1 && (
+              <ActionIcon
+                size={'1.8rem'}
+                mb={'0.2rem'}
+                color="red.6"
+                onClick={() => form.removeListItem('options', i)}
+              >
+                <IconTrash />
+              </ActionIcon>
+            )}
+          </Group>
+        ))}
+        <Button
+          mt={'0.5rem'}
+          variant="light"
+          className={classes.addOptionBtn}
+          onClick={() => form.insertListItem('options', { value: '' })}
+        >
+          Add option
+        </Button>
 
-      <DateTimePicker
-        label="Expires at"
-        clearable
-        {...form.getInputProps('expiresAt')}
-      />
+        <Accordion className={classes.accordion}>
+          <Accordion.Item value="Settings">
+            <Accordion.Control>Settings</Accordion.Control>
+            <Accordion.Panel>
+              <DateTimePicker
+                label="Expires at"
+                clearable
+                {...form.getInputProps('expiresAt')}
+              />
 
-      <Checkbox
-        label="Private"
-        description="private polls are only accessible by link"
-        {...form.getInputProps('isPrivate')}
-      />
-      <Select
-        label="Results visibility"
-        data={['always', 'after_vote', 'after_deadline']}
-        defaultValue="always"
-        {...form.getInputProps('resultsVisibility')}
-      />
-      <Button type="submit">Submit</Button>
-    </form>
+              <Checkbox
+                mt={'1rem'}
+                mb={'1rem'}
+                label="Private"
+                description="private polls are only accessible by link"
+                {...form.getInputProps('isPrivate')}
+              />
+              <Select
+                label="Results visibility"
+                data={['always', 'after_vote', 'after_deadline']}
+                defaultValue="always"
+                {...form.getInputProps('resultsVisibility')}
+              />
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+        <Button className={classes.submitBtn} size="compact-xl" type="submit">
+          Submit
+        </Button>
+      </form>
+    </>
   );
 };
 
