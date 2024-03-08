c12e64c1100813f3722981afa41d42c46a6f8931
ivica <blizujemars@gmail.com>
2024-03-08 17:58:01 +0100

    add desc and expires refs

--- src/hooks/useEdit.ts
+++ src/hooks/useEdit.ts
@@ -1,10 +1,14 @@
 import { useRef } from 'react';
 
 const useEdit = () => {
-  const questionRef = useRef<HTMLInputElement>(null);
+  const questionRef = useRef<HTMLTextAreaElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const expiresRef = useRef<HTMLButtonElement>(null);
 
   return {
     questionRef,
+    descriptionRef,
+    expiresRef,
   };
 };
 
