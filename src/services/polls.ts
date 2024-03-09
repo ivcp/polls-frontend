746d6381d47ab048ea9828712422acad5d1f9331
ivica <blizujemars@gmail.com>
2024-03-09 10:33:20 +0100

    refactor

--- src/services/polls.ts
+++ src/services/polls.ts
@@ -7,53 +7,8 @@ import {
   EditPollBody,
 } from '../types';
 
-const listPolls = async (page: number): Promise<PollsResponse> => {
-  const response = await fetch(`/v1/polls${'?page_size=12&page=' + page}`);
-  if (!response.ok) {
-    const err: { error: string } = await response.json();
-    throw new Error(err.error);
-  }
-  return await response.json();
-};
-
-const getPoll = async (pollID: string): Promise<PollResponse> => {
-  const response = await fetch(`/v1/polls/${pollID}`);
-  if (!response.ok) {
-    const err: { error: string } = await response.json();
-    throw new Error(err.error);
-  }
-  return await response.json();
-};
-
-const vote = async (
-  pollID: string,
-  optionID: string
-): Promise<VoteResponse> => {
-  const response = await fetch(`/v1/polls/${pollID}/options/${optionID}`, {
-    method: 'POST',
-  });
-  if (!response.ok) {
-    const err: { error: string } = await response.json();
-    throw new Error(err.error);
-  }
-  return await response.json();
-};
-
-const getResults = async (pollID: string): Promise<VoteResults> => {
-  const response = await fetch(`/v1/polls/${pollID}/results`);
-  if (!response.ok) {
-    const err: { error: string } = await response.json();
-    throw new Error(err.error);
-  }
-  return await response.json();
-};
-
-const createPoll = async (poll: CreatePollBody): Promise<PollResponse> => {
-  const response = await fetch('/v1/polls', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(poll),
-  });
+const fetchData = async (url: string, config: RequestInit | undefined) => {
+  const response = await fetch(url, config);
   if (!response.ok) {
     if (response.status === 422) {
       const err: {
@@ -69,13 +24,41 @@ const createPoll = async (poll: CreatePollBody): Promise<PollResponse> => {
       for (const entry of Object.entries(err.error)) {
         errors += `"${entry[0]}": ${entry[1]}|`;
       }
-
       throw new Error(errors);
     }
     const err: { error: string } = await response.json();
     throw new Error(err.error);
   }
   return await response.json();
+};
+
+const listPolls = async (page: number): Promise<PollsResponse> => {
+  return await fetchData(`/v1/polls${'?page_size=12&page=' + page}`, undefined);
+};
+
+const getPoll = async (pollID: string): Promise<PollResponse> => {
+  return await fetchData(`/v1/polls/${pollID}`, undefined);
+};
+
+const vote = async (
+  pollID: string,
+  optionID: string
+): Promise<VoteResponse> => {
+  return await fetchData(`/v1/polls/${pollID}/options/${optionID}`, {
+    method: 'POST',
+  });
+};
+
+const getResults = async (pollID: string): Promise<VoteResults> => {
+  return await fetchData(`/v1/polls/${pollID}/results`, undefined);
+};
+
+const createPoll = async (poll: CreatePollBody): Promise<PollResponse> => {
+  return await fetchData('/v1/polls', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(poll),
+  });
 };
 
 const editPoll = async (
@@ -83,7 +66,7 @@ const editPoll = async (
   editPollBody: EditPollBody,
   token: string
 ): Promise<PollResponse> => {
-  const response = await fetch(`/v1/polls/${pollID}`, {
+  return await fetchData(`/v1/polls/${pollID}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
@@ -91,28 +74,6 @@ const editPoll = async (
     },
     body: JSON.stringify(editPollBody),
   });
-  if (!response.ok) {
-    if (response.status === 422) {
-      const err: {
-        error: {
-          expires_at?: string;
-          options?: string;
-          question?: string;
-          description?: string;
-          results_visibility?: string;
-        };
-      } = await response.json();
-      let errors = '';
-      for (const entry of Object.entries(err.error)) {
-        errors += `"${entry[0]}": ${entry[1]}|`;
-      }
-
-      throw new Error(errors);
-    }
-    const err: { error: string } = await response.json();
-    throw new Error(err.error);
-  }
-  return await response.json();
 };
 
 export default {
